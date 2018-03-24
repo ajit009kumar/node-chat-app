c63be1ab5ae29bc9256014afe6cea203436a2f59
ajit009kumar <ajit009kumar@gmail.com>
2018-03-24 19:33:10 +0530

    add geolocationsupport using geolocationapi

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const { generateMessage } = require('./utils/message');
+const { generateMessage ,generateLocationMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname , '../public');
 var app = express();
@@ -45,6 +45,10 @@ io.on('connection',(socket) => {
         // })
 
      });
+
+     socket.on('createLocationMessage',(coords) => {
+         io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+     })
 
      socket.on('disconnect',() => {
          console.log('User Was Disconnected ');
