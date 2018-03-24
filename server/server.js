4038ffd79eb97e09a008b5ce3b68a73f684f264d
ajit009kumar <ajit009kumar@gmail.com>
2018-03-24 16:36:18 +0530

    greet new user and alert other

--- server/server.js
+++ server/server.js
@@ -23,6 +23,18 @@ io.on('connection',(socket) => {
     //     console.log('createEmail', newEmail);
     //  });
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()        
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
      socket.on('createMessage',(newMessage) => {
         console.log('createMessage', newMessage);
         io.emit('newMessage',{
@@ -30,6 +42,13 @@ io.on('connection',(socket) => {
             text: newMessage.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage',{
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+
+        // })
 
      });
 
